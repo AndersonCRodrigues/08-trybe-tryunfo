a17a7f32721d0295c9bdf891c1829263eaced2ad
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-10-26 16:33:02 -0300

    adiciona validação do botão "Salvar"

--- src/App.js
+++ src/App.js
@@ -20,17 +20,37 @@ class App extends React.Component {
     cardAttr2: '',
     cardAttr3: '',
     cardImage: '',
-    cardRare: '',
+    cardRare: 'normal',
     cardTrunfo: false,
     hasTrunfo: false,
-    isSaveButtonDisabled: false,
+    isSaveButtonDisabled: true,
+  };
+
+  handleIsDisable = () => {
+    const { cardName, cardDescription, cardImage,
+      cardRare, cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const maxTotal = 210;
+    const maxAttr = 90;
+    const minAttr = 0;
+    const total = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3);
+
+    const textCheck = cardName && cardDescription && cardImage && cardRare;
+
+    const attrCheck = (total <= maxTotal);
+
+    const attt1MaxCheck = (Number(cardAttr1) <= maxAttr && Number(cardAttr1) >= minAttr);
+    const attt2MaxCheck = (Number(cardAttr2) <= maxAttr && Number(cardAttr2) >= minAttr);
+    const attt3MaxCheck = (Number(cardAttr3) <= maxAttr && Number(cardAttr3) >= minAttr);
+    const minMaxAttrCheck = attt1MaxCheck && attt2MaxCheck && attt3MaxCheck;
+
+    this.setState({ isSaveButtonDisabled: !(textCheck && attrCheck && minMaxAttrCheck) });
   };
 
   onInputChange = ({ target }) => {
     const { name, value, type, checked } = target;
     const valor = type === 'checkbox' ? checked : value;
 
-    this.setState({ [name]: valor });
+    this.setState({ [name]: valor }, (this.handleIsDisable));
   };
 
   render() {
